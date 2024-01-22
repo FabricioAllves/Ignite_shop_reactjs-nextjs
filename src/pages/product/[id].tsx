6df60f6229ef723250193ce67ea72e9a02ed364c
FabricioAllves <fabricioohh@gmail.com>
2024-01-21 23:35:00 -0300

    first commit

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+
+export default function Product() {
+  const { query } = useRouter()
+
+  return (
+    <h1>
+      Products: {JSON.stringify(query)}
+    </h1>
+  );
+}
